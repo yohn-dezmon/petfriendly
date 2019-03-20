e00f300f96c8119d9d8d8248427903bf741fd04c
Johnboi <johndesmond631@gmail.com>
2019-03-19 23:01:35 -0500

    added a form to enter email and name of fans. I also added layout.html as a parent html template and extended it with jinja

--- pet_back_end.py
+++ pet_back_end.py
@@ -1,24 +1,48 @@
 # importing the flask class
 from flask import Flask, render_template, request, send_from_directory
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/pre-registration'
+# db = SQLAlchemy(app)
+#
+# class User(db.Model):
+#     __tablename__ = "fans"
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True)
+#     name = db.Column(db.string(200), unique=False)
+#
+#     def __init__(self, email, name):
+#         self.email = email
+#         self.name = name
+# # repr returns '<E-mail email, name name>'
+# # what's up with the <> ??
+#     def __repr__(self):
+#         return '<E-mail {self.email}, name {self.name}>'.format(self=self)
 
 # http://localhost:5000/
 @app.route('/', methods=['POST', 'GET'])
 def home():
-
     song_dict = {"Pumba":'PUMBA_1.mp3', "You're Feet":'youre feet mix 1.8.17_1.mp3',
      "ratatatatata":'ratatat.mp3',
     "12 | 8":'12_8 NO CLICK.mp3', "Sage to Your Internet" : 'sage to your internet .mp3',
     "EB":'EB DEMO MIX NO CLICK.mp3'}
+    email = None
+    name = None
+    if request.method == "POST":
+        email = request.form['email'] # this sets python email equal to the input named 'email'
+        name = request.form['name']
+        thank_u = "thank you!! <3"
+        return render_template("thank_u.html", thank_u=thank_u,
+                                song_dict=song_dict)
+    else:
+        return render_template("pet_friend.html",
+                                song_dict = song_dict
+                                )
 
 
 
 
-    return render_template("pet_friend.html",
-                            song_dict = song_dict
-                            )
 
 
 
